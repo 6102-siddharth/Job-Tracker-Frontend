4a6a892bdf37c294b1742684b52805ec95bb8ab5
6102-siddharth <siddharth.vadgaonkar06@gmail.com>
2026-01-22 17:13:34 +0530

    Path is given

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import axios from "axios";
 import JobCard from "../src/Components/JobCard.jsx";
 import ResumeUpload from "../src/Components/ResumeUpload.jsx";
 import Dashboard from "../src/Components/Dashboard.jsx";
+const API_URL = "https://job-tracker-backend-56f8.onrender.com";
+
 
 export default function App() {
   const [jobs, setJobs] = useState([]);
   const [resume, setResume] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:5000/jobs").then(res => setJobs(res.data));
-    axios.get("http://localhost:5000/resume")
+    axios.get($`{API_URL}/jobs`).then(res => setJobs(res.data));
+    axios.get($`{API_URL}/resume`)
     .then(res => setResume(res.data.resumeText))
     .catch(err => console.error(err));
   }, []);
